6a5c64eea09ec1855389a07ea685ccbbc0aa1ba8
MarcosQuintino0 <marcosquintinodev@gmail.com>
2024-06-21 17:01:19 -0300

    sorteador de numero por fala

--- project/project/public/app/app.js
+++ project/project/public/app/app.js
@@ -1,21 +1,31 @@
 // app/app.js
-import { log } from './utils/promise-helpers.js';
-import './utils/array-helpers.js';
-import { notasService as service } from './nota/service.js';
-import { takeUntil, debounceTime, partialize, pipe } from './utils/operators.js';
+import { log } from "./utils/promise-helpers.js";
+import "./utils/array-helpers.js";
+import { notasService as service } from "./nota/service.js";
+import {
+  takeUntil,
+  debounceTime,
+  partialize,
+  pipe,
+} from "./utils/operators.js";
+
+const p1 = new Promise((resolve, rejct) =>
+  setTimeout(() => (resolve) => "promisse 1 terminou", 3000)
+);
+
+const p2 = new Promise((resolve, rejct) =>
+  setTimeout(() => (resolve) => "promisse 2 terminou", 1000)
+);
+
+Promise.race([p1, p2]).then(console.log).catch(console.log);
 
 const operations = pipe(
-    partialize(takeUntil, 3),
-    partialize(debounceTime, 500)
+  partialize(takeUntil, 3),
+  partialize(debounceTime, 500)
 );
 
-const action = operations(() => 
-    service
-    .sumItems('2143')
-    .then(console.log)
-    .catch(console.log)
+const action = operations(() =>
+  service.sumItems("2143").then(console.log).catch(console.log)
 );
 
-document
-.querySelector('#myButton')
-.onclick = action;
+document.querySelector("#myButton").onclick = action;
