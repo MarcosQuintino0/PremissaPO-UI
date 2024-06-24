0d8ccc08c9acddec93ba0c6bf965590c9ef49b19
MarcosQuintino0 <marcosquintinodev@gmail.com>
2024-06-24 11:19:23 -0300

    reduce e sort

--- js-metodos-array-aula_1/app/metedoForEach.js
+++ js-metodos-array-aula_1/app/metedoForEach.js
@@ -1,10 +1,16 @@
 const elementoParaInserirLivros = document.getElementById("livros");
+const elementoComValorTotalDeLivrosDisponiveis = document.getElementById(
+  "valor_total_livros_disponiveis"
+);
 
 function exibirOsLivrosNaTela(listaDeLivros) {
+  elementoParaInserirLivros.innerHTML = "";
   listaDeLivros.forEach((livro) => {
+    let disponibilidade =
+      livro.quantidade > 0 ? "livro__imagens" : "livros__imagens indisponivel";
     elementoParaInserirLivros.innerHTML += `
         <div class="livro">
-        <img class="livro__imagens" src="${livro.imagem}"
+        <img class="${disponibilidade}" src="${livro.imagem}"
           alt="${livro.alt}" />
         <h2 class="livro__titulo">
           ${livro.titulo}
